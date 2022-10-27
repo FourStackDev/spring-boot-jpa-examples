057abb791b32802c02ebf4bf711f88cdc84656af
FourStackDev <fourstackdevelopers@gmail.com>
2022-10-27 15:26:37 +0530

    Added Initial commit for voter-api app

---
+++ voter-api/src/main/java/org/manjunath/voterapi/VoterApiApplication.java
@@ -0,0 +1,13 @@
+package org.manjunath.voterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoterApiApplication.class, args);
+	}
+
+}
---
+++ voter-api/src/test/java/org/manjunath/voterapi/VoterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.manjunath.voterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
