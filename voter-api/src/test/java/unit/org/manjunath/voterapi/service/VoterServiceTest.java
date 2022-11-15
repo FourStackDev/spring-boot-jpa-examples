515bb8d540138d750bb395fa758352277b48dbb8
FourStackDev <fourstackdevelopers@gmail.com>
2022-11-15 16:31:58 +0530

    Added new test cases for service layer

--- voter-api/src/test/java/unit/org/manjunath/voterapi/service/VoterServiceTest.java
+++ voter-api/src/test/java/unit/org/manjunath/voterapi/service/VoterServiceTest.java
@@ -10,6 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import util.EntityGenerator;
 
 import java.util.List;
@@ -60,6 +63,74 @@ public class VoterServiceTest {
         Assertions.assertIterableEquals(voters, voterList);
 
         // verify the method has been called or not
-        Mockito. verify(repository).findByFirstNameIgnoreCase(Mockito.anyString());
+        Mockito.verify(repository).findByFirstNameIgnoreCase(Mockito.anyString());
+    }
+
+    @Test
+    @DisplayName("VoterServiceTest: Test to get Voter Page by firstname")
+    public void testGetVotersPageByFirstName() {
+        String firstName = "Sunil";
+        List<Voter> voters = EntityGenerator.getVoters().stream()
+                .filter(voter -> voter.getFirstName().equals(firstName))
+                .collect(Collectors.toList());
+
+        Page<Voter> page = new PageImpl<>(voters);
+
+        // mock the repository layer
+        Mockito.when(repository.findByFirstNameIgnoreCase(Mockito.anyString(), Mockito.any(Pageable.class)))
+                .thenReturn(page);
+
+        // invoke the target method and verify the result
+        Page<Voter> voterPage = service.getVotersPageByFirstName(firstName, 0, 10);
+        Assertions.assertEquals(voters.size(), voterPage.getContent().size());
+        Assertions.assertIterableEquals(voters, voterPage.getContent());
+
+        //verify the method has been called or not
+        Mockito.verify(repository, Mockito.times(1))
+                .findByFirstNameIgnoreCase(Mockito.anyString(), Mockito.any(Pageable.class));
+    }
+
+    @Test
+    @DisplayName("VoterServiceTest: Test to get Voters by lastname")
+    public void testGetAllVotersByLastName() {
+        String lastname = "Bhuyar";
+        List<Voter> voters = EntityGenerator.getVoters().stream()
+                .filter(voter -> voter.getLastName().equals(lastname))
+                .collect(Collectors.toList());
+
+        // mock the repository layer
+        Mockito.when(repository.findByLastNameIgnoreCase(Mockito.anyString()))
+                .thenReturn(voters);
+
+        // invoke the target method and verify the result
+        List<Voter> voterList = service.getAllVotersByLastName(lastname);
+        Assertions.assertEquals(voters.size(), voterList.size());
+        Assertions.assertIterableEquals(voters, voterList);
+
+        //verify the method has been called or not
+        Mockito.verify(repository).findByLastNameIgnoreCase(Mockito.anyString());
+    }
+
+    @Test
+    @DisplayName("VoterServiceTest: Test to get Voter page by lastname")
+    public void testGetVotersPageByLastName() {
+        String lastname = "HM";
+        List<Voter> voters = EntityGenerator.getVoters().stream()
+                .filter(voter -> voter.getLastName().equals(lastname))
+                .collect(Collectors.toList());
+        Page<Voter> page = new PageImpl<>(voters);
+
+        // mock the repository layer
+        Mockito.when(repository.findByLastNameIgnoreCase(Mockito.anyString(), Mockito.any(Pageable.class)))
+                .thenReturn(page);
+
+        // invoke the target method and verify the result
+        Page<Voter> voterPage = service.getVotersPageByLastName(lastname, 0, 10);
+        Assertions.assertEquals(voters.size(), voterPage.getContent().size());
+        Assertions.assertIterableEquals(voters, voterPage.getContent());
+
+        // verify the method has been called or not
+        Mockito.verify(repository, Mockito.times(1))
+                .findByLastNameIgnoreCase(Mockito.anyString(), Mockito.any(Pageable.class));
     }
 }
